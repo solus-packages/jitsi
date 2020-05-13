16d2d67342b13c1ebcfe807cb886c2c49af5e4e8
Fabio Forni <fabio@redaril.me>
2020-05-13 02:39:15 -0400

    Update jitsi to 5633 and make it use openjdk-8
    Summary: No changelog available.
    
    Test Plan: Successfully ran jitsi and connected to Freenode.
    
    Reviewers: #triage_team, DataDrake
    
    Reviewed By: #triage_team, DataDrake
    
    Subscribers: DataDrake
    
    Differential Revision: https://dev.getsol.us/D8713

--- files/jitsi.sh
+++ files/jitsi.sh
@@ -1,15 +1,14 @@
-#!/bin/bash
-
+#!/bin/sh
 
 SCDIR=/usr/share/jitsi
 LIBPATH="$SCDIR/lib"
-CLASSPATH="$LIBPATH/jdic_stub.jar:$LIBPATH/jdic-all.jar:$LIBPATH/felix.jar:$LIBPATH/bcprovider.jar:$SCDIR/sc-bundles/sc-launcher.jar:$SCDIR/sc-bundles/util.jar"
+CLASSPATH="${LIBPATH}/felix.jar:${SCDIR}/sc-bundles/sc-launcher.jar:${SCDIR}/sc-bundles/util.jar:${SCDIR}/sc-bundles/dnsjava.jar:${LIBPATH}"
 FELIX_CONFIG="$LIBPATH/felix.client.run.properties"
 LOG_CONFIG="$LIBPATH/logging.properties"
-COMMAND=(java -classpath "$CLASSPATH" -Djna.library.path="$LIBPATH/native" -Dfelix.config.properties="file:$FELIX_CONFIG" -Djava.util.logging.config.file="$LOG_CONFIG" net.java.sip.communicator.launcher.SIPCommunicator)
+
+COMMAND=(/usr/lib/openjdk-8/bin/java -classpath "$CLASSPATH" -Djna.library.path="$LIBPATH/native" -Dfelix.config.properties="file:$FELIX_CONFIG" -Djava.util.logging.config.file="$LOG_CONFIG" net.java.sip.communicator.launcher.SIPCommunicator)
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$LIBPATH/native"
 
 cd "$SCDIR"
-
 exec "${COMMAND[@]}" "$@"
