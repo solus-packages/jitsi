8bbc671735c09f4ca13dc6c4582edb19ab9c5880
Campbell Jones <git@serebit.com>
2020-06-13 17:33:18 -0400

    Use openjdk-8 to build jitsi with ant
    Summary: This is necessary as of the inclusion of JDK11.
    
    Test Plan: Opened jitsi
    
    Reviewers: #triage_team, DataDrake
    
    Reviewed By: #triage_team, DataDrake
    
    Subscribers: DataDrake
    
    Maniphest Tasks: T6658
    
    Differential Revision: https://dev.getsol.us/D9021

--- files/jitsi.sh
+++ files/jitsi.sh
@@ -6,7 +6,11 @@ CLASSPATH="${LIBPATH}/felix.jar:${SCDIR}/sc-bundles/sc-launcher.jar:${SCDIR}/sc-
 FELIX_CONFIG="$LIBPATH/felix.client.run.properties"
 LOG_CONFIG="$LIBPATH/logging.properties"
 
-COMMAND=(/usr/lib/openjdk-8/bin/java -classpath "$CLASSPATH" -Djna.library.path="$LIBPATH/native" -Dfelix.config.properties="file:$FELIX_CONFIG" -Djava.util.logging.config.file="$LOG_CONFIG" net.java.sip.communicator.launcher.SIPCommunicator)
+if [ -z "$JAVA_HOME" ]; then
+    export JAVA_HOME=/usr/lib64/openjdk-8
+fi
+
+COMMAND=($JAVA_HOME/bin/java -classpath "$CLASSPATH" -Djna.library.path="$LIBPATH/native" -Dfelix.config.properties="file:$FELIX_CONFIG" -Djava.util.logging.config.file="$LOG_CONFIG" net.java.sip.communicator.launcher.SIPCommunicator)
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$LIBPATH/native"
 
